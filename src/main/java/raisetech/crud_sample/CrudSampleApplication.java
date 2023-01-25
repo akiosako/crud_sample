5e378e5842317f06761c7eef88f9debc398ab488
akiosako <vaticano2662@gmail.com>
2023-01-25 21:27:53 +0900

    first commit

---
+++ src/main/java/raisetech/crud_sample/CrudSampleApplication.java
@@ -0,0 +1,13 @@
+package raisetech.crud_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/raisetech/crud_sample/CrudSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package raisetech.crud_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

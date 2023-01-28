0dc2401b246f1a6f4d26815c7bd8c60f9a5d7fc7
akiosako <vaticano2662@gmail.com>
2023-01-28 21:22:40 +0900

    改行、フォーマット修正

--- src/main/java/raisetech/crud_sample/controller/Controller.java
+++ src/main/java/raisetech/crud_sample/controller/Controller.java
@@ -15,61 +15,62 @@ import java.net.URI;
 import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.Map;
+
 @RestController
 @RequestMapping("/msg")
 public class Controller {
-        private final MsgService msgService;
+    private final MsgService msgService;
 
-        public Controller(MsgService msgService){
-            this.msgService = msgService;
-        }
+    public Controller(MsgService msgService) {
+        this.msgService = msgService;
+    }
 
-        @GetMapping
-        public List<Message> findAll(){
-            return msgService.findAll();
-        }
+    @GetMapping
+    public List<Message> findAll() {
+        return msgService.findAll();
+    }
 
-        @GetMapping("/{id}")//idが存在しない場合は404を返す
-        public Message findById(@PathVariable int id){
-            return msgService.findById(id);
-        }
+    @GetMapping("/{id}")//idが存在しない場合は404を返す
+    public Message findById(@PathVariable int id) {
+        return msgService.findById(id);
+    }
 
-        @PostMapping //30文字を超える場合はエラーメッセージを返す
-        public ResponseEntity<String> createMsg(@RequestBody @Valid MsgForm msgForm, UriComponentsBuilder uriComponentsBuilder){
-            String newId = toString().valueOf(msgService.createMsg(msgForm));
-            URI uri = uriComponentsBuilder.path("msg/{newId}")
-                    .buildAndExpand(newId)
-                    .toUri();
-            return ResponseEntity.created(uri).body("message successfully created");
+    @PostMapping //30文字を超える場合はエラーメッセージを返す
+    public ResponseEntity<String> createMsg(@RequestBody @Valid MsgForm msgForm, UriComponentsBuilder uriComponentsBuilder) {
+        String newId = toString().valueOf(msgService.createMsg(msgForm));
+        URI uri = uriComponentsBuilder.path("msg/{newId}")
+                .buildAndExpand(newId)
+                .toUri();
+        return ResponseEntity.created(uri).body("message successfully created");
 
-        }
+    }
 
-        @PatchMapping("/{id}")//idが存在しない場合は404を返す
-        public ResponseEntity<String> updateMsg(@PathVariable int id,@RequestBody UpdateForm updateForm){
-            msgService.findById(id);
-            updateForm.setId(id);
-            msgService.updateMsg(updateForm);
-            return ResponseEntity.ok("message successfully updated");
-        }
+    @PatchMapping("/{id}")//idが存在しない場合は404を返す
+    public ResponseEntity<String> updateMsg(@PathVariable int id, @RequestBody UpdateForm updateForm) {
+        msgService.findById(id);
+        updateForm.setId(id);
+        msgService.updateMsg(updateForm);
+        return ResponseEntity.ok("message successfully updated");
+    }
 
-        @DeleteMapping("/{id}")//idが存在しない場合は404を返す
-        public ResponseEntity<String> deleteMsg(@PathVariable int id){
-            msgService.findById(id);
-            msgService.deleteMsg(id);
-            return ResponseEntity.ok("message successfully deleted");
-        }
+    @DeleteMapping("/{id}")//idが存在しない場合は404を返す
+    public ResponseEntity<String> deleteMsg(@PathVariable int id) {
+        msgService.findById(id);
+        msgService.deleteMsg(id);
+        return ResponseEntity.ok("message successfully deleted");
+    }
 
-        @ExceptionHandler(value = ResourceNotFoundException.class)
-        public ResponseEntity<Map<String,String>> handleNoResourceFound(ResourceNotFoundException e,
-                                                                        HttpServletRequest request){
-            Map<String,String> body = Map.of(
-                    "timestamp", ZonedDateTime.now().toString(),
-                    "Status", String.valueOf(HttpStatus.NOT_FOUND.value()),
-                    "error",HttpStatus.NOT_FOUND.getReasonPhrase(),
-                    "message",e.getMessage(),
-                    "path",request.getRequestURI());
-            return new ResponseEntity(body, HttpStatus.NOT_FOUND);
-        }
+    @ExceptionHandler(value = ResourceNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleNoResourceFound(ResourceNotFoundException e,
+                                                                     HttpServletRequest request) {
+        Map<String, String> body = Map.of(
+                "timestamp", ZonedDateTime.now().toString(),
+                "Status", String.valueOf(HttpStatus.NOT_FOUND.value()),
+                "error", HttpStatus.NOT_FOUND.getReasonPhrase(),
+                "message", e.getMessage(),
+                "path", request.getRequestURI());
+        return new ResponseEntity(body, HttpStatus.NOT_FOUND);
+    }
 }
 
 
--- src/main/java/raisetech/crud_sample/controller/ResourceNotFoundException.java
+++ src/main/java/raisetech/crud_sample/controller/ResourceNotFoundException.java
@@ -1,22 +1,19 @@
 package raisetech.crud_sample.controller;
 
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(){
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException() {
         super();
     }
 
-    public ResourceNotFoundException(String message, Throwable cause){
-        super(message,cause);
+    public ResourceNotFoundException(String message, Throwable cause) {
+        super(message, cause);
     }
 
-    public ResourceNotFoundException(String message){
+    public ResourceNotFoundException(String message) {
         super(message);
     }
 
-    public ResourceNotFoundException(Throwable cause){
+    public ResourceNotFoundException(Throwable cause) {
         super(cause);
     }
 }
-
-
-

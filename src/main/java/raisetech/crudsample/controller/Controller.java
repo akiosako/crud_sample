885501d75d001c41829a1921e5b698db599e64ec
akiosako <vaticano2662@gmail.com>
2023-04-07 16:05:53 +0900

    application.propertiesの記載を削除し、リクエストボディのバリデーションエラーを実装

--- src/main/java/raisetech/crudsample/controller/Controller.java
+++ src/main/java/raisetech/crudsample/controller/Controller.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 import raisetech.crudsample.entity.Message;
@@ -21,6 +24,7 @@ import raisetech.crudsample.service.MsgService;
 
 import java.net.URI;
 import java.time.ZonedDateTime;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -79,6 +83,18 @@ public class Controller {
             "message", e.getMessage(),
             "path", request.getRequestURI());
     return new ResponseEntity(body, HttpStatus.NOT_FOUND);
+  }
+
+  @ExceptionHandler(value = MethodArgumentNotValidException.class)
+  @ResponseBody
+  public ResponseEntity<Map<String, Object>> handleMethodArgumentNotValidExceptionError(MethodArgumentNotValidException e, HttpServletRequest request) {
+    Map<String, String> body = Map.of(
+            "timestamp", ZonedDateTime.now().toString(),
+            "Status", String.valueOf(HttpStatus.BAD_REQUEST.value()),
+            "error", HttpStatus.BAD_REQUEST.getReasonPhrase(),
+            "message", "21文字以上のメッセージ、空文字、nullは許容されません。",
+            "path", request.getRequestURI());
+    return new ResponseEntity(body, HttpStatus.BAD_REQUEST);
   }
 }
 
